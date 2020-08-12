a897d0b53ca54cf42046115ad635c05e1b85bfca
EduerPJ <eduerpj@gmail.com>
2020-08-12 14:01:03 -0500

    add php-database-crud

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Models\Job;
-require_once 'jobs.php';
-use App\Models\Project as Project;
+require_once('jobs.php');
 
 $name = 'Eduer Pallares';
-$limitMonts = 2000;
+$limitMonths = 2000;
 
 ?>
 
@@ -17,8 +16,9 @@ $limitMonts = 2000;
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="index_files/bootstrap.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-  <link rel="stylesheet" href="index_files/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 
   <title>Resume</title>
 </head>
@@ -27,10 +27,10 @@ $limitMonts = 2000;
   <div class="container">
     <div id="resume-header" class="row">
       <div class="col-3">
-        <img id="profile-picture" src="index_files/a.png" alt="">
+        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1>Hector Benitez</h1>
+        <h1><?php echo $name; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
@@ -42,43 +42,76 @@ $limitMonts = 2000;
     </div>
     <div class="row">
       <div class="col">
-        <h2 class="border-bottom-gray">Carrer Summary</h2>
+        <h2 class="border-bottom-gray" >Carrer Summary</h2>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
-do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
-do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
     </div>
     <div class="row">
       <div class="col">
         <div>
-          <h3 class="border-bottom-gray">Work Experience</h3>
+          <h3 class="border-bottom-gray" >Work Experience</h3>
           <ul>
-          <?php
-            require_once 'jobs.php';
-          ?>
-          <ul>
-               
+            <?php
+            $totalMonths = 0;
+            for ($idx = 0;$idx < count($jobs); $idx++) {
+                // $totalMonths = $totalMonths + $jobs[$idx]['months'];
+                $totalMonths += $jobs[$idx]->months;
+                if ($totalMonths > $limitMonths) {
+                    break;
+                }
+
+                printElement($jobs[$idx]);
+            }
+            ?>
+          </ul>
         </div>
         <div>
             <h3 class="border-bottom-gray">Projects</h3>
+            <ul>
+            <?php
+            for ($idx = 0;$idx < count($projects); $idx++) {
+                printElement($projects[$idx]);
+            }
+            ?>
+          </ul>
 
-            <?php require_once 'lib1/Projects.php'; ?>
-
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+            <div class="project">
+                <h5>Project X</h5>
+                <div class="row">
+                    <div class="col-3">
+                        <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
+                      </div>
+                      <div class="col">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius earum corporis at accusamus quisquam hic quos vel? Tenetur, ullam veniam consequatur esse quod cum, quam cupiditate assumenda natus maiores aperiam.</p>
+                        <strong>Technologies used:</strong>
+                        <span class="badge badge-secondary">PHP</span>
+                        <span class="badge badge-secondary">HTML</span>
+                        <span class="badge badge-secondary">CSS</span>
+                      </div>
+                </div>
+            </div>
+          </div>
+      </div>
       <div class="col-3">
-        <h3 class="border-bottom-gray">Skills &amp; Tools</h3>
+        <h3 class="border-bottom-gray" >Skills & Tools</h3>
         <h4>Backend</h4>
         <ul>
           <li>PHP</li>
@@ -93,7 +126,7 @@ officia deserunt mollit anim id est laborum.
           <li>Laravel</li>
           <li>Bootstrap</li>
         </ul>
-        <h3 class="border-bottom-gray">Languages</h3>
+        <h3 class="border-bottom-gray" >Languages</h3>
         <ul>
           <li>Spanish</li>
           <li>English</li>
@@ -109,9 +142,12 @@ officia deserunt mollit anim id est laborum.
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="index_files/jquery-3.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="index_files/popper.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="index_files/bootstrap.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+    crossorigin="anonymous"></script>
+</body>
 
-
-</body></html>
+</html>
